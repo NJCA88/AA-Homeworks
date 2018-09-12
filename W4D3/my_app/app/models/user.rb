2347390efb39ca42f5f358d49bc8ef7c31c3baaf
Chris Atwood <coachchris289@gmail.com>
2018-09-11 21:56:06 -0700

    Almost all of auth

--- W4D3/my_app/app/models/user.rb
+++ W4D3/my_app/app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
   validates :username, :session_token, presence: true
-  validates :password_digest, presence: (message: 'Password cant be blank')
+  validates :password_digest, presence: {message: 'Password cant be blank'}
   validates :password, length:{ minimum: 6, allow_nil: true}
   before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username)
+    return user if user && Bcrypt::Password.new(user.password).is_password?(password)
+    nil
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token=Uuser.generate_session_token
+    self.save!
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+  
+  def password=
+  end
+
+
 end
