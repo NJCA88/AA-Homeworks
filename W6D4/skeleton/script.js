01acd4841ca4ddec97311983e8427d2cc0e6c59a
Chris Atwood <coachchris289@gmail.com>
2018-09-26 22:06:01 -0700

    Vanilla DOM hw

---
+++ W6D4/skeleton/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+
+    const handleFavoriteSubmit = (e) => {
+  e.preventDefault();
+
+  const favoriteInput = document.querySelector(".favorite-input");
+  const favorite = favoriteInput.value;
+  favoriteInput.value = "";
+
+  const newListLi = document.createElement("li");
+  newListLi.textContent = favorite;
+
+  const favoritesList = document.getElementById("sf-places");
+  favoritesList.appendChild(newListLi);
+  });
+
+
+
+  // adding SF places as list items
+
+  // --- your code here!
+
+
+
+  // adding new photos
+
+  // --- your code here!
+
+
+
+});
